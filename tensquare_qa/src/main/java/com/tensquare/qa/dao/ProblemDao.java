7ba352abd0b7140d6cbb914d56b3aaa93f3ead23
DESKTOP-B42P468\GYX <904985041@qq.com>
2018-12-12 16:24:10 +0800

    增加redis和文章模块

--- tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -1,14 +1,24 @@
 package com.tensquare.qa.dao;
 
+import com.tensquare.qa.pojo.Problem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-import com.tensquare.qa.pojo.Problem;
 /**
  * 数据访问接口
- * @author Administrator
  *
+ * @author Administrator
  */
-public interface ProblemDao extends JpaRepository<Problem,String>,JpaSpecificationExecutor<Problem>{
-	
+public interface ProblemDao extends JpaRepository<Problem, String>, JpaSpecificationExecutor<Problem> {
+    @Query(value = "SELECT * FROM tb_problem,tb_pl WHERE id = problemid AND labelid = ? ORDER BY replytime DESC", nativeQuery = true)
+    Page<Problem> newlist(String labelid, Pageable pageable);
+
+    @Query(value = "SELECT * FROM tb_problem,tb_pl WHERE id = problemid AND labelid = ? ORDER BY reply DESC", nativeQuery = true)
+    Page<Problem> hotlist(String labelid, Pageable pageable);
+
+    @Query(value = "SELECT * FROM tb_problem,tb_pl WHERE id = problemid AND labelid = ? AND reply = 0 ORDER BY createtime DESC", nativeQuery = true)
+    Page<Problem> waitlist(String labelid, Pageable pageable);
 }
